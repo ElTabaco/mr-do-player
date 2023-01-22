65e5a7e07f4804d1db532d748140e2142ace9253
ElTabaco <70294424+ElTabaco@users.noreply.github.com>
2023-01-22 23:25:27 +0100

    Update push.sh
    docker login before docker push needed
    Workaround no automated solution yet

--- mr-do-snapserver/push.sh
+++ mr-do-snapserver/push.sh
@@ -4,5 +4,6 @@ ARCH=arm64
 echo ${SNAPCAST_VERSION}
 
 #docker image push --all-tags riemerk/mr-do-snapserver
+docker login -u "myusername" -p "mypassword" docker.io
 docker push riemerk/mr-do-snapserver:latest
 docker push riemerk/mr-do-snapserver:${SNAPCAST_VERSION}-ARCH=${ARCH}
