810674c3f5d5dfcfebd9e76b3f4ebd3f1c177ffd
Konstantin <git>
2025-01-18 21:06:09 +0100

    remove sudo

--- mr-do-player/kubernetes/apply.sh
+++ mr-do-player/kubernetes/apply.sh
@@ -1,35 +1,29 @@
-sudo kubectl create namespace mr-do-player
-sudo kubectl apply -f mr-do-player-pv.yml
-sudo kubectl apply -f mr-do-player-pvc.yml
-sudo kubectl create configmap mr-do-snapserver-cfgmap --from-file=etc/snapserver.conf -n mr-do-player
-# sudo kubectl get configmap mr-do-snapserver-cfgmap -o yaml  -n mr-do-player
-sudo kubectl create configmap mr-do-asound-cfgmap --from-file=etc/asound.conf -n mr-do-player
-# sudo kubectl get configmap mr-do-asound-cfgmap -o yaml  -n mr-do-player
-sudo kubectl apply -f mr-do-player-service.yml
-sudo kubectl apply -f mr-do-player-deployment.yml
-#sudo kubectl apply -f mr-do-player-ingress.yml
+kubectl create namespace mr-do-player
+kubectl apply -f mr-do-player-pv.yml
+kubectl apply -f mr-do-player-pvc.yml
+kubectl create configmap mr-do-snapserver-cfgmap --from-file=etc/snapserver.conf -n mr-do-player
+# kubectl get configmap mr-do-snapserver-cfgmap -o yaml  -n mr-do-player
+kubectl create configmap mr-do-asound-cfgmap --from-file=etc/asound.conf -n mr-do-player
+# kubectl get configmap mr-do-asound-cfgmap -o yaml  -n mr-do-player
+kubectl apply -f mr-do-player-service.yml
+kubectl apply -f mr-do-player-deployment.yml
+# kubectl apply -f mr-do-player-ingress.yml
 
-sudo kubectl describe pod mr-do-player -n mr-do-player
-sudo kubectl get pods -n mr-do-player -o wide
-sudo kubectl get pods --all-namespaces -o wide
+kubectl describe pod mr-do-player -n mr-do-player
+kubectl get pods -n mr-do-player -o wide
+kubectl get pods --all-namespaces -o wide
 
+# kubectl get ingress --all-namespaces -o wide
+# kubectl describe ingress -n mr-do-player
 
+kubectl get pv --all-namespaces -o wide
+kubectl get pvc --all-namespaces -o wide
+kubectl describe pv mr-do-player-pv-data -n mr-do-player
+kubectl describe pvc mr-do-player-pvc-data -n mr-do-player
 
-#sudo kubectl get ingress --all-namespaces -o wide
-#sudo kubectl describe ingress -n mr-do-player
+kubectl get configmap --all-namespaces -o wide
+kubectl get svc --all-namespaces
+kubectl get services  -n mr-do-player -o wide
+kubectl describe services mr-do-player-service -n mr-do-player
 
-sudo kubectl get pv --all-namespaces -o wide
-sudo kubectl get pvc --all-namespaces -o wide
-
-
-sudo kubectl describe pv mr-do-player-pv-data -n mr-do-player
-sudo kubectl describe pvc mr-do-player-pvc-data -n mr-do-player
-
-sudo kubectl get configmap --all-namespaces -o wide
-
-sudo kubectl get svc --all-namespaces
-sudo kubectl get services  -n mr-do-player -o wide
-sudo kubectl describe services mr-do-player-service -n mr-do-player
-
-sudo kubectl get all -n mr-do-player
-
+kubectl get all -n mr-do-player
