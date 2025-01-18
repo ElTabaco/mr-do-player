d9022212db3ac176a75a300b17ac5a413909049d
Konstantin <git>
2025-01-18 21:05:43 +0100

    remove not needed root comands

--- mr-do-player/kubernetes/delete.sh
+++ mr-do-player/kubernetes/delete.sh
@@ -1,10 +1,10 @@
-sudo kubectl delete -f mr-do-player-deployment.yml
-sudo kubectl delete -f mr-do-player-service.yml
-sudo kubectl delete configmap mr-do-asound-cfgmap -n mr-do-player
-#sudo kubectl delete configmap mr-do-kodi-cfgmap -n mr-do-player
-sudo kubectl delete configmap mr-do-snapserver-cfgmap -n mr-do-player
-sudo kubectl delete -f mr-do-player-pvc.yml
-sudo kubectl delete -f mr-do-player-pv.yml
-#sudo kubectl delete -f mr-do-player-ingress.yml
-sudo kubectl delete all,deployment,configmap,pv,pvc --all -n mr-do-player
-sudo kubectl delete namespace mr-do-player
+kubectl delete -f mr-do-player-deployment.yml
+kubectl delete -f mr-do-player-service.yml
+kubectl delete configmap mr-do-asound-cfgmap -n mr-do-player
+# kubectl delete configmap mr-do-kodi-cfgmap -n mr-do-player
+kubectl delete configmap mr-do-snapserver-cfgmap -n mr-do-player
+kubectl delete -f mr-do-player-pvc.yml
+kubectl delete -f mr-do-player-pv.yml
+# kubectl delete -f mr-do-player-ingress.yml
+# kubectl delete all,deployment,configmap,pv,pvc --all -n mr-do-player
+kubectl delete namespace mr-do-player
